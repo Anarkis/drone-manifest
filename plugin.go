79d6ee84fa037c61d25fe7cf7012b1b13c13fcec
Luis Tovar <luis.tovar@suse.coM>
2022-09-21 10:50:24 +0200

    Enabling publicecr

--- plugin.go
+++ plugin.go
@@ -75,20 +75,17 @@ func mainfestToolPath() string {
 func (p *Plugin) Exec() error {
 	args := []string{}
 
-	// if !p.Config.Publicecr {
-	// if p.Config.Username == "" && p.Config.Password != "" {
-	// return errors.New("you must provide a username")
-	// } else {
-	// args = append(args, fmt.Sprintf("--username=%s", p.Config.Username))
-	// }
-	// } else {
-	// args = append(args, "--docker-cfg='/root/.docker/'")
-	// }
-
-	if p.Config.Password == "" && p.Config.Username != "" {
-		return errors.New("you must provide a password")
-	} else {
-		args = append(args, fmt.Sprintf("--password=%s", p.Config.Password))
+	if !p.Config.Publicecr {
+		if p.Config.Username == "" && p.Config.Password != "" {
+			return errors.New("you must provide a username")
+		} else {
+			args = append(args, fmt.Sprintf("--username=%s", p.Config.Username))
+		}
+		if p.Config.Password == "" && p.Config.Username != "" {
+			return errors.New("you must provide a password")
+		} else {
+			args = append(args, fmt.Sprintf("--password=%s", p.Config.Password))
+		}
 	}
 
 	if p.Config.Insecure {
